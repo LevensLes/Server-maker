00a90261b8d9cf424b3043ebb8eb0af88298745e
LevensLes <32346039+LevensLes@users.noreply.github.com>
2018-06-17 07:39:11 +0200

    Add files via upload

---
+++ Server Creator/Form1.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.FormSkin1 = New Server_Creator.FormSkin()
+        Me.FlatLabel5 = New Server_Creator.FlatLabel()
+        Me.FlatCheckBox2 = New Server_Creator.FlatCheckBox()
+        Me.FlatLabel4 = New Server_Creator.FlatLabel()
+        Me.FlatComboBox2 = New Server_Creator.FlatComboBox()
+        Me.FlatLabel3 = New Server_Creator.FlatLabel()
+        Me.FlatLabel2 = New Server_Creator.FlatLabel()
+        Me.FlatLabel1 = New Server_Creator.FlatLabel()
+        Me.FlatCheckBox1 = New Server_Creator.FlatCheckBox()
+        Me.FlatClose1 = New Server_Creator.FlatClose()
+        Me.FlatComboBox1 = New Server_Creator.FlatComboBox()
+        Me.FlatButton1 = New Server_Creator.FlatButton()
+        Me.FormSkin1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FlatLabel5)
+        Me.FormSkin1.Controls.Add(Me.FlatCheckBox2)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel4)
+        Me.FormSkin1.Controls.Add(Me.FlatComboBox2)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel3)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel2)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel1)
+        Me.FormSkin1.Controls.Add(Me.FlatCheckBox1)
+        Me.FormSkin1.Controls.Add(Me.FlatClose1)
+        Me.FormSkin1.Controls.Add(Me.FlatComboBox1)
+        Me.FormSkin1.Controls.Add(Me.FlatButton1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(72, Byte), Integer), CType(CType(61, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(227, 259)
+        Me.FormSkin1.TabIndex = 0
+        Me.FormSkin1.Text = "FormSkin1"
+        '
+        'FlatLabel5
+        '
+        Me.FlatLabel5.AutoSize = True
+        Me.FlatLabel5.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel5.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FlatLabel5.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel5.Location = New System.Drawing.Point(27, 202)
+        Me.FlatLabel5.Name = "FlatLabel5"
+        Me.FlatLabel5.Size = New System.Drawing.Size(73, 13)
+        Me.FlatLabel5.TabIndex = 11
+        Me.FlatLabel5.Text = "Accept eula"
+        '
+        'FlatCheckBox2
+        '
+        Me.FlatCheckBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FlatCheckBox2.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatCheckBox2.BorderColor = System.Drawing.Color.FromArgb(CType(CType(72, Byte), Integer), CType(CType(61, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.FlatCheckBox2.Checked = True
+        Me.FlatCheckBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatCheckBox2.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.FlatCheckBox2.Location = New System.Drawing.Point(3, 197)
+        Me.FlatCheckBox2.Name = "FlatCheckBox2"
+        Me.FlatCheckBox2.Options = Server_Creator.FlatCheckBox._Options.Style1
+        Me.FlatCheckBox2.Size = New System.Drawing.Size(112, 22)
+        Me.FlatCheckBox2.TabIndex = 10
+        '
+        'FlatLabel4
+        '
+        Me.FlatLabel4.AutoSize = True
+        Me.FlatLabel4.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel4.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel4.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel4.Location = New System.Drawing.Point(48, 106)
+        Me.FlatLabel4.Name = "FlatLabel4"
+        Me.FlatLabel4.Size = New System.Drawing.Size(29, 13)
+        Me.FlatLabel4.TabIndex = 8
+        Me.FlatLabel4.Text = "Ram"
+        '
+        'FlatComboBox2
+        '
+        Me.FlatComboBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.FlatComboBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatComboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.FlatComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.FlatComboBox2.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatComboBox2.ForeColor = System.Drawing.Color.White
+        Me.FlatComboBox2.FormattingEnabled = True
+        Me.FlatComboBox2.HoverColor = System.Drawing.Color.FromArgb(CType(CType(72, Byte), Integer), CType(CType(61, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.FlatComboBox2.ItemHeight = 18
+        Me.FlatComboBox2.Items.AddRange(New Object() {"1GB", "2GB", "3GB", "4GB"})
+        Me.FlatComboBox2.Location = New System.Drawing.Point(83, 103)
+        Me.FlatComboBox2.Name = "FlatComboBox2"
+        Me.FlatComboBox2.Size = New System.Drawing.Size(121, 24)
+        Me.FlatComboBox2.TabIndex = 7
+        Me.FlatComboBox2.Tag = ""
+        '
+        'FlatLabel3
+        '
+        Me.FlatLabel3.AutoSize = True
+        Me.FlatLabel3.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel3.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel3.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel3.Location = New System.Drawing.Point(3, 76)
+        Me.FlatLabel3.Name = "FlatLabel3"
+        Me.FlatLabel3.Size = New System.Drawing.Size(78, 13)
+        Me.FlatLabel3.TabIndex = 6
+        Me.FlatLabel3.Text = "Server version"
+        '
+        'FlatLabel2
+        '
+        Me.FlatLabel2.AutoSize = True
+        Me.FlatLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel2.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FlatLabel2.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel2.Location = New System.Drawing.Point(6, 16)
+        Me.FlatLabel2.Name = "FlatLabel2"
+        Me.FlatLabel2.Size = New System.Drawing.Size(105, 15)
+        Me.FlatLabel2.TabIndex = 5
+        Me.FlatLabel2.Text = "Server Creator"
+        '
+        'FlatLabel1
+        '
+        Me.FlatLabel1.AutoSize = True
+        Me.FlatLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FlatLabel1.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel1.Location = New System.Drawing.Point(26, 230)
+        Me.FlatLabel1.Name = "FlatLabel1"
+        Me.FlatLabel1.Size = New System.Drawing.Size(109, 13)
+        Me.FlatLabel1.TabIndex = 4
+        Me.FlatLabel1.Text = "Basic 1.7 plugins"
+        '
+        'FlatCheckBox1
+        '
+        Me.FlatCheckBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FlatCheckBox1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatCheckBox1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(72, Byte), Integer), CType(CType(61, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.FlatCheckBox1.Checked = False
+        Me.FlatCheckBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatCheckBox1.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FlatCheckBox1.Location = New System.Drawing.Point(3, 225)
+        Me.FlatCheckBox1.Name = "FlatCheckBox1"
+        Me.FlatCheckBox1.Options = Server_Creator.FlatCheckBox._Options.Style1
+        Me.FlatCheckBox1.Size = New System.Drawing.Size(37, 22)
+        Me.FlatCheckBox1.TabIndex = 3
+        '
+        'FlatClose1
+        '
+        Me.FlatClose1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatClose1.BackColor = System.Drawing.Color.White
+        Me.FlatClose1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.FlatClose1.Font = New System.Drawing.Font("Marlett", 10.0!)
+        Me.FlatClose1.Location = New System.Drawing.Point(186, 13)
+        Me.FlatClose1.Name = "FlatClose1"
+        Me.FlatClose1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatClose1.TabIndex = 2
+        Me.FlatClose1.Text = "FlatClose1"
+        Me.FlatClose1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatComboBox1
+        '
+        Me.FlatComboBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.FlatComboBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.FlatComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.FlatComboBox1.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatComboBox1.ForeColor = System.Drawing.Color.White
+        Me.FlatComboBox1.FormattingEnabled = True
+        Me.FlatComboBox1.HoverColor = System.Drawing.Color.FromArgb(CType(CType(72, Byte), Integer), CType(CType(61, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.FlatComboBox1.ItemHeight = 18
+        Me.FlatComboBox1.Items.AddRange(New Object() {"1.7.10", "1.8.9", "1.9", "1.10", "1.11", "1.12"})
+        Me.FlatComboBox1.Location = New System.Drawing.Point(83, 73)
+        Me.FlatComboBox1.Name = "FlatComboBox1"
+        Me.FlatComboBox1.Size = New System.Drawing.Size(121, 24)
+        Me.FlatComboBox1.TabIndex = 1
+        Me.FlatComboBox1.Tag = ""
+        '
+        'FlatButton1
+        '
+        Me.FlatButton1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(72, Byte), Integer), CType(CType(61, Byte), Integer), CType(CType(139, Byte), Integer))
+        Me.FlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton1.Location = New System.Drawing.Point(143, 215)
+        Me.FlatButton1.Name = "FlatButton1"
+        Me.FlatButton1.Rounded = False
+        Me.FlatButton1.Size = New System.Drawing.Size(72, 32)
+        Me.FlatButton1.TabIndex = 0
+        Me.FlatButton1.Text = "Start"
+        Me.FlatButton1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(227, 259)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.FormSkin1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FormSkin1 As FormSkin
+    Friend WithEvents FlatComboBox1 As FlatComboBox
+    Friend WithEvents FlatButton1 As FlatButton
+    Friend WithEvents FlatClose1 As FlatClose
+    Friend WithEvents FlatLabel1 As FlatLabel
+    Friend WithEvents FlatCheckBox1 As FlatCheckBox
+    Friend WithEvents FlatLabel2 As FlatLabel
+    Friend WithEvents FlatLabel3 As FlatLabel
+    Friend WithEvents FlatLabel4 As FlatLabel
+    Friend WithEvents FlatComboBox2 As FlatComboBox
+    Friend WithEvents FlatLabel5 As FlatLabel
+    Friend WithEvents FlatCheckBox2 As FlatCheckBox
+End Class
---
+++ Server Creator/Form1.vb
@@ -0,0 +1,135 @@
+﻿Imports System.IO
+
+
+Public Class Form1
+
+
+    Dim UserAccName As String = SystemInformation.UserName
+    Dim Ram1 As String = ("Xms1024M -Xmx1024M")
+    Dim Ram2 As String = ("Xmx2048M -Xms2048M")
+    Dim Ram3 As String = ("Xmx3072M -Xms3072M")
+    Dim Ram4 As String = ("Xmx4096M -Xms4096M")
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FlatComboBox1.SelectedIndex = 0
+        FlatComboBox2.SelectedIndex = 1
+
+    End Sub
+
+    Private Sub FlatClose1_Click(sender As Object, e As EventArgs) Handles FlatClose1.Click
+        Close()
+    End Sub
+
+    Private Sub FlatButton1_Click(sender As Object, e As EventArgs) Handles FlatButton1.Click
+
+        If FlatCheckBox1.Checked = True Then
+
+        End If
+
+
+        If Directory.Exists("C:\Users\" & UserAccName & "\desktop\Server") Then
+            MessageBox.Show("Folder already exists")
+            Stop
+        Else
+            Directory.CreateDirectory("C:\Users\" & UserAccName & "\desktop\Server")
+        End If
+
+        If FlatComboBox1.Text.Contains("1.7.10") Then
+            My.Computer.Network.DownloadFile("https://cdn.getbukkit.org/spigot/spigot-1.7.10-SNAPSHOT-b1657.jar", "C:\Users\" & UserAccName & "\desktop\Server\spigot.jar")
+        End If
+
+        If FlatComboBox1.Text.Contains("1.8.9") Then
+            My.Computer.Network.DownloadFile("https://cdn.getbukkit.org/spigot/spigot-1.8-R0.1-SNAPSHOT-latest.jar", "C:\Users\" & UserAccName & "\desktop\Server\spigot.jar")
+        End If
+
+        If FlatComboBox1.Text.Contains("1.9") Then
+            My.Computer.Network.DownloadFile("https://cdn.getbukkit.org/spigot/spigot-1.9-R0.1-SNAPSHOT-latest.jar", "C:\Users\" & UserAccName & "\desktop\Server\spigot.jar")
+        End If
+
+        If FlatComboBox1.Text.Contains("1.10") Then
+            My.Computer.Network.DownloadFile("https://cdn.getbukkit.org/spigot/spigot-1.10-R0.1-SNAPSHOT-latest.jar", "C:\Users\" & UserAccName & "\desktop\Server\spigot.jar")
+        End If
+
+        If FlatComboBox1.Text.Contains("1.11") Then
+            My.Computer.Network.DownloadFile("https://cdn.getbukkit.org/spigot/spigot-1.11.jar", "C:\Users\" & UserAccName & "\desktop\Server\spigot.jar")
+        End If
+
+        If FlatComboBox1.Text.Contains("1.12") Then
+            My.Computer.Network.DownloadFile("https://cdn.getbukkit.org/spigot/spigot-1.12.jar", "C:\Users\" & UserAccName & "\desktop\Server\spigot.jar")
+        End If
+
+        Dim filepath As String = ("C:\Users\" & UserAccName & "\desktop\Server\start.bat")
+        System.IO.File.Create(filepath).Dispose()
+
+
+        Dim file As System.IO.StreamWriter
+
+        If FlatComboBox2.Text.Contains("1GB") Then
+            file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\" & UserAccName & "\desktop\Server\start.bat", True)
+            file.WriteLine("@echo off
+java -" & Ram1 & " -jar spigot.jar -o true 
+pause")
+            file.Close()
+        End If
+
+        If FlatComboBox2.Text.Contains("2GB") Then
+            file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\" & UserAccName & "\desktop\Server\start.bat", True)
+            file.WriteLine("@echo off
+java -" & Ram2 & " -jar spigot.jar -o true 
+pause")
+            file.Close()
+        End If
+
+        If FlatComboBox2.Text.Contains("3GB") Then
+            file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\" & UserAccName & "\desktop\Server\start.bat", True)
+            file.WriteLine("@echo off
+java -" & Ram3 & " -jar spigot.jar -o true 
+pause")
+            file.Close()
+        End If
+
+        If FlatComboBox2.Text.Contains("4GB") Then
+            file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\" & UserAccName & "\desktop\Server\start.bat", True)
+            file.WriteLine("@echo off
+java -" & Ram4 & " -jar spigot.jar -o true 
+pause")
+            file.Close()
+        End If
+
+        My.Computer.Network.DownloadFile("https://www.levensles.club/server/run.bat",
+          "C:\Users\" & UserAccName & "\Downloads\run.bat")
+        Dim proc As New System.Diagnostics.Process()
+
+        proc = Process.Start("C:\Users\" & UserAccName & "\Downloads\run.bat", "")
+
+        Threading.Thread.Sleep(1600)
+
+
+        If FlatCheckBox2.Checked = True Then
+
+
+            Dim nigger As String = ("C:\Users\" & UserAccName & "\desktop\Server\eula.txt")
+            System.IO.File.Create(nigger).Dispose()
+
+            file = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\" & UserAccName & "\desktop\Server\eula.txt", True)
+            file.WriteLine("#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
+#Sun Jun 17 02:42:37 CEST 2018
+eula=true
+")
+            file.Close()
+
+        Else
+
+        End If
+
+        If FlatCheckBox1.Checked = True Then
+            My.Computer.Network.DownloadFile("https://www.levensles.club/server/worldedit-bukkit-6.0.jar",
+          "C:\Users\" & UserAccName & "\desktop\Server\plugins\world-edit.jar")
+
+            My.Computer.Network.DownloadFile("https://www.levensles.club/server/Essentials.jar",
+         "C:\Users\" & UserAccName & "\desktop\Server\plugins\essentials.jar")
+
+        End If
+
+    End Sub
+End Class
